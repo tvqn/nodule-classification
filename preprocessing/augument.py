ae8d42431355277fcb8d1248db19599033d753a1
whale <thinkandstep@gmail.com>
2020-03-07 10:22:15 +0700

    update

--- preprocessing/augument.py
+++ preprocessing/augument.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import permutations, combinations
 
-def rotate_3D(cube):
-    lstCube = []
+def permute_3D(cube):
     for x in permutations([0, 1, 2], 3):
-        lstCube.append(cube.transpose(x))
-    return lstCube
+        yield cube.transpose(x)
